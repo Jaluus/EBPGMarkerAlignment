1162963b3f621834f7bdde5dd03afe37f1724ef6
Jan-Lucas Uslu <janlucas.uslu@gmail.com>
2025-07-25 13:37:34 -0700

    Updated Image

--- alignment_debug.py
+++ alignment_debug.py
@@ -102,13 +102,14 @@ def main():
         image = to_int8(image)
 
         # Remove gradient intensity by fitting and subtracting a plane
-        h, w = image.shape
+        test_image = image.copy().astype(np.float32)
+        h, w = test_image.shape
         y, x = np.mgrid[0:h, 0:w]
         A = np.column_stack([x.ravel(), y.ravel(), np.ones(x.size)])
-        coeffs, _, _, _ = np.linalg.lstsq(A, image.ravel(), rcond=None)
-        plane = (coeffs[0] * x + coeffs[1] * y + coeffs[2]).astype(image.dtype)
-        image = image - plane
-        image = np.clip(image, 0, 255).astype(np.uint8)
+        coeffs, _, _, _ = np.linalg.lstsq(A, test_image.ravel(), rcond=None)
+        plane = (coeffs[0] * x + coeffs[1] * y + coeffs[2]).astype(test_image.dtype)
+        test_image = test_image - plane
+        test_image = np.clip(test_image, 0, 255).astype(np.uint8)
 
         # normalize image to 0-255 for visualization
         # plot the histogram of the image
